35ee65a3caa104e38f27f15a021eddf9892ee0dc
Anton Belov <belovanton.deal@gmail.com>
2025-06-14 17:01:21 +0300

    Работает взаимодействие сервисов с запуском докер компоуз.

--- user-service/src/main/java/com/example/user_service/client/NotificationServiceClient.java
+++ user-service/src/main/java/com/example/user_service/client/NotificationServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "notification-service", url = "${notification.service.url}/api/notification-service")
+@FeignClient(name = "notification-service", url = "http://notification-service:8082/api/notification-service")
 public interface NotificationServiceClient {
     @PostMapping("/sign-up-notification")
     ResponseEntity<NotificationEntity> signUpNotification(@RequestBody UserDTO userDTO);
